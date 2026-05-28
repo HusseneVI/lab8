e1adefa1ff3c11d4a6bfce88b75e2ce0f02f66af
HusseneVI <2024155477@estudantes.ips.pt>
2026-05-28 02:31:49 +0100

    todos niveis

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,59 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("=== Lab 8 - ClassGroup tests ===");
+
+        ClassGroup group = createGroup();
+
+        System.out.println("\nInitial students:");
+
+        printStudents(group);
+
+        System.out.println("\nAverage grade:");
+        System.out.println(group.avgGrade());
+
+        System.out.println("\nStudents with grade greater than 14:");
+        System.out.println(group.countGrades(14));
+
+        System.out.println("\nStudents sorted by name:" + group.sortByName());
+
+
+        System.out.println("\nTop 3 students:" + group.top3());
+
+
+        System.out.println("\nHas students with grade lower than 9.5?");
+        System.out.println(group.hasLowerGrade(9.5));
+
+        System.out.println("\nDo all student numbers have 5 digits?");
+        System.out.println(group.validNumbers());
+
+        System.out.println("\nNames of the students sorted by name with number of enrollements > 2:");
+        System.out.println(group.namesNumberOfEnrolledments(2));
+
+        System.out.println("\nRemove students with more than 2 enrollments using Iterator:");
+
+        ClassGroup groupIterator = createGroup();
+        groupIterator.removeByEnrollments(2);
+        printStudents(groupIterator);
+    }
+
+    private static ClassGroup createGroup() {
+        ClassGroup group = new ClassGroup("POO");
+
+        group.add(new Student(12345, "Ana Silva", 18.5, 1));
+        group.add(new Student(23456, "Bruno Costa", 12.0, 2));
+        group.add(new Student(34567, "Carla Sousa", 16.0, 1));
+        group.add(new Student(45678, "Diogo Martins", 9.0, 4));
+        group.add(new Student(56789, "Eva Pereira", 14.5, 3));
+        group.add(new Student(67890, "Filipe Rocha", 19.0, 1));
+        group.add(new Student(78901, "Gabriela Lopes", 11.5, 5));
+        group.add(new Student(89012, "Henrique Ramos", 15.0, 2));
+
+        return group;
+
+    }
+
+    private static void printStudents(ClassGroup group) {
+        group.getStudents().forEach(System.out::println);
     }
 }
--- src/main/java/org/example/ClassGroup.java
+++ src/main/java/org/example/ClassGroup.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class ClassGroup {
 
@@ -12,6 +16,69 @@ public class ClassGroup {
         this.name = name;
         this.students = new ArrayList<>();
     }
+
+    //Nivel 1
+    public void removeByEnrollments(int x) {
+        Iterator<Student> it = students.iterator();
+
+        while (it.hasNext()) {
+            Student student = it.next();
+
+            if (student.getEnrollments() > x) {
+                it.remove();
+            }
+        }
+    }
+
+    //Nivel 2
+    public long countGrades(double value) {
+        return students.stream()
+                .filter(students -> students.getGrade() > value)
+                .count();
+    }
+
+    public List<Student> sortByName() {
+        return students.stream()
+                .sorted(Comparator.comparing(Student::getName))
+                .toList();
+    }
+
+    //Nivel 3
+    public List<Student> top3() {
+        return students.stream()
+                .sorted(Comparator.comparing(Student::getGrade)).limit(3)
+                .toList();
+    }
+
+    public double avgGrade() {
+        return students.stream()
+                .mapToDouble(Student::getGrade)
+                .average()
+                .orElse(0.0);
+    }
+
+    //Nivel 4
+    public boolean hasLowerGrade(double value) {
+        return students.stream()
+                .anyMatch(student -> student.getGrade() < value);
+    }
+
+    public boolean validNumbers() {
+        return students.stream().allMatch(student -> student.getNumber() >= 10000
+                && student.getNumber() <= 99999);
+    }
+
+    public List<String> namesNumberOfEnrolledments(int number) {
+        return students.stream()
+                .filter(s -> s.getEnrollments() == number)
+                .map(n -> n.getName().toUpperCase())
+                .sorted()
+                .toList();
+
+    }
+
+
+
 
     public void add(Student student) {
         students.add(student);
@@ -24,6 +91,7 @@ public class ClassGroup {
     public String getName() {
         return name;
     }
+
 
     @Override
     public String toString() {
